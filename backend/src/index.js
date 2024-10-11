3b8007fcd912e9e0185ab4f5926d29db122797de
Vinicius Ribeiro <vinii.viniciusribeiro@gmail.com>
2024-10-11 20:35:55 -0300

    Starting with te Express Framework abd creating development scripts to launch the application

---
+++ backend/src/index.js
@@ -0,0 +1,9 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/tasks", (request, response) =>
+  response.status(200).send("Hello, Word")
+);
+
+app.listen(3333, () => console.log("Server running on port 3333"));

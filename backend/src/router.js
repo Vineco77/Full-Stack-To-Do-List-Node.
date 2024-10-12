850f3d35cf4bc3c35bbd15e19541032510f7302b
Vinicius Ribeiro <vinii.viniciusribeiro@gmail.com>
2024-10-12 01:08:28 -0300

    Creating validators for the request to set a new task

--- backend/src/router.js
+++ backend/src/router.js
@@ -1,9 +1,11 @@
 const express = require("express");
 
-const tasksController = require("./controllers/taskController");
-
 const router = express.Router();
 
+const tasksController = require("./controllers/taskController");
+const tasksMiddleware = require("./middlewares/tasksMiddleware");
+
 router.get("/tasks", tasksController.getAll);
+router.post("/tasks", tasksMiddleware.validateBody, tasksController.createTask);
 
 module.exports = router;

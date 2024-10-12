780bca452dad6643e0329ca1202a66ae7781a527
Vinicius Ribeiro <vinii.viniciusribeiro@gmail.com>
2024-10-11 21:11:32 -0300

    Creating environment variables for the MySQL databese

--- backend/src/index.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-
-const app = express();
-
-app.get("/tasks", (request, response) =>
-  response.status(200).send("Hello, Word")
-);
-
-app.listen(3333, () => console.log("Server running on port 3333"));
